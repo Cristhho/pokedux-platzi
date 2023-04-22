925fbef696efd6ddebe23bac3300ba3d95623aa8
christian <cristhho@gmail.com>
2023-04-21 23:35:35 -0500

    set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Col, Layout, Row, Typography } from 'antd';
 
-function App() {
+import { Searcher } from './components';
+
+const  App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout.Content style={{ paddingTop: '1.5rem' }}>
+      <Typography.Title level={3}>
+        Pokedux
+      </Typography.Title>
+      <Row>
+        <Col span={8} offset={8}>
+          <Searcher />
+        </Col>
+      </Row>
+    </Layout.Content>
   );
 }
 
